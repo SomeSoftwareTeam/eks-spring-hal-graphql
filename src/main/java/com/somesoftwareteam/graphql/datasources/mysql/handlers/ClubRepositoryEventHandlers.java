b6c85c760556a85e22854a57ad21e50736bd801e
Thomas Portwood <thomas.w.portwood@gmail.com>
2020-08-07 20:20:30 -0400

    change club handler to AFTER create

--- src/main/java/com/somesoftwareteam/graphql/datasources/mysql/handlers/ClubRepositoryEventHandlers.java
+++ src/main/java/com/somesoftwareteam/graphql/datasources/mysql/handlers/ClubRepositoryEventHandlers.java
@@ -4,6 +4,7 @@ import com.somesoftwareteam.graphql.datasources.auth0.Auth0ExtGroup;
 import com.somesoftwareteam.graphql.datasources.auth0.Auth0Wrapper;
 import com.somesoftwareteam.graphql.datasources.mysql.entities.Club;
 import com.somesoftwareteam.graphql.security.AuthenticationFacade;
+import org.springframework.data.rest.core.annotation.HandleAfterCreate;
 import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
 import org.springframework.data.rest.core.annotation.HandleBeforeSave;
 import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
@@ -26,9 +27,9 @@ public class ClubRepositoryEventHandlers {
         this.auth0Wrapper = auth0Wrapper;
     }
 
-    @HandleBeforeCreate
-    public void handleBeforeCreate(Club club) throws IOException {
-        Auth0ExtGroup newGroup = auth0Wrapper.createAuth0ExtGroup(club.getName(), club.getDescription());
+    @HandleAfterCreate
+    public void handleAfterCreate(Club club) throws IOException {
+        Auth0ExtGroup newGroup = auth0Wrapper.createAuth0ExtGroup(club.getId().toString(), club.getDescription());
         String principal = authenticationFacade.getCurrentPrincipalName();
         auth0Wrapper.addUserToAuth0ExtGroup(newGroup, principal);
     }
